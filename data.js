b4c516fcf0b50d2c3e41683a26ff91992efa0757
Hubert OG <hubert@orlikarnia.com>
2015-12-15 14:42:39 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
+Template.closestData = function(key) {
+  var i = 0;
+  var data = Template.parentData(i);
+  while(data) {
+    if(data.hasOwnProperty(key)) return data;
+    i += 1;
+    data = Template.parentData(i);
+  }
+  return null;
+};
+
+Template.closestDataFetch = function(key) {
+  var data = Template.closestData(key);
+  return data ? data[key] : null;
+};
+
+Template.viewDataMarker = '_thisIsViewData';
+
+Template.viewData = function() {
+  return Template.closestData(Template.viewDataMarker);
+};
+
 
