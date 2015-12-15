22aed3f1833047584f6d61b6de0895a2c64b1c6a
Hubert OG <hubert@orlikarnia.com>
2015-12-15 14:41:36 +0100

    Update components.js

--- components.js
+++ components.js
@@ -1,1 +1,40 @@
+'use strict';
+
+Template.prototype.component = function (name, value) {
+  this.components(_.object([ name ], [ value ]));
+};
+
+Template.prototype.components = function (options) {
+
+  var helpers = {};
+
+  this.onCreated(function () {
+    var instance = this;
+  
+    _.each(options, function (value, name) {
+      if(!instance.components) {
+        instance.components = {};
+      }
+      if (typeof value === 'function') {
+        instance.components[name] = value.call(instance);
+      } else {
+        instance.components[name] = value;
+      }
+    });
+  });
+
+  _.each(_.keys(options), function (name) {
+    helpers[name] = function () {
+      return Template.instance().components[name];
+    };
+  });
+
+  this.helpers(helpers);
+};
+
+Template.component = function (name) {
+  var instance = Template.instance();
+  return instance.components && instance.components[name];
+};
+
 
